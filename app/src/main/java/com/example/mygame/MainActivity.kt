5c291a36248269522c58f61a5a6d646169e5ce46
David Montes <david.montes@sjsu.edu>
2020-02-08 13:45:52 -0800

    Added Action Bar

--- app/src/main/java/com/example/mygame/MainActivity.kt
+++ app/src/main/java/com/example/mygame/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.mygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.mygame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
